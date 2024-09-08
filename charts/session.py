1e64f6a529573b031977ef7c9e1ecd3284ae94ea
Rammuni28 <117463378+Rammuni28@users.noreply.github.com>
2024-09-09 01:34:31 +0530

    database password update

--- charts/session.py
+++ charts/session.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from decouple import config
 
-DATABASE_URL = f"mysql+pymysql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}/{config('DB_NAME')}"
+DATABASE_URL = f"mysql+pymysql://{config('DB_USER')}:{config('DB_PASSWORD').replace('@', '%40')}@{config('DB_HOST')}/{config('DB_NAME')}"
 engine = create_engine(DATABASE_URL)
 Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
