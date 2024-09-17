19bb4e0e7bde0292148321eef9010ed9818a48bc
Rammuni28 <117463378+Rammuni28@users.noreply.github.com>
2024-09-17 16:18:38 +0530

    Delete charts directory

--- charts/session.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from decouple import config
-from urllib.parse import quote_plus
-
-encoded_password = quote_plus(config('DB_PASSWORD'))
-DATABASE_URL = f"mysql+pymysql://{config('DB_USER')}:{encoded_password}@{config('DB_HOST')}/{config('DB_NAME')}"
-engine = create_engine(DATABASE_URL)
-Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
